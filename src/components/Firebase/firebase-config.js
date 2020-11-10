5c6eb034434bfeab2e1dcf2ddb18dbd369adbb45
kartikdutt18 <kartikdutt@live.in>
2020-11-10 18:15:55 +0530

    Created React based workout management app

---
+++ src/components/Firebase/firebase-config.js
@@ -0,0 +1,12 @@
+const config = {
+  apiKey: "AIzaSyAkrok1Zrf-51ZX3-PdfRdyeaiPkbTt6oU",
+  authDomain: "workout-manager-87f8d.firebaseapp.com",
+  databaseURL: "https://workout-manager-87f8d.firebaseio.com",
+  projectId: "workout-manager-87f8d",
+  storageBucket: "workout-manager-87f8d.appspot.com",
+  messagingSenderId: "672248640665",
+  appId: "1:672248640665:web:8296195cec7c8ac0c0b408",
+  measurementId: "G-BE0ZZRM9QW"
+};
+
+export default config;
